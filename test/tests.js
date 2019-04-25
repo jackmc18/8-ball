4ffcd933cf3b53ce7727c5d13e6d45c5d7876bc0
Jackmc <jackmc1122@gmail.com>
2019-04-24 17:48:07 -0700

    Add a custom assertion to test if the answer is from within the array of answers.

--- test/tests.js
+++ test/tests.js
@@ -1,15 +1,32 @@
 const test = QUnit.test;
 
+// Make a custom assertion
+QUnit.assert.contains = function(needle, haystack, message) {
+  var actual = haystack.indexOf(needle) > -1;
+  this.push(actual, actual, needle, message);
+};
+
 const answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',
   'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
   'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don`t count on it.', 'My reply is no.',
   'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
-test('time to test a function', function(assert) {
+function ballAnswer(question) {
+  if(question) {
+    let random = Math.floor(Math.random() * 20); 
+    return answers[random];
+  }
+  return;
+}
+
+test('Send a question and expect an answer from the array of answers', function(assert) {
   //Arrange
   // Set up your parameters and expectations
+  const question = 'How will my day go?';
+  const expected = answers;
   //Act 
+  const result = ballAnswer(question);
   // Call the function you're testing and set the result to a const
   //Assert
-  assert.equal(true, false);
+  assert.contains(result, expected);
 });
