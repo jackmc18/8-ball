eb6cebbcae3105567d4f6005d001cdb87e9cd19e
Jackmc <jackmc1122@gmail.com>
2019-04-24 18:47:07 -0700

    Update the message with the answer from the 8ball.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import { ballAnswer } from './ball-answer.js';
+
 const questionInput = document.getElementById('question');
 const submitButton = document.getElementById('submit');
+const answerMessage = document.getElementById('message');
 
 submitButton.addEventListener('click', () => {
-  console.log(questionInput.value);
+  const question = questionInput.value;
+  const answer = ballAnswer(question);
+  answerMessage.textContent = 'Your answer: ' + answer;
 });
