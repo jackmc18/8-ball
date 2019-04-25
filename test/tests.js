6e6c6acc6cae6e7d11a3e9f90dfc3a9d62739c2a
Jackmc <jackmc1122@gmail.com>
2019-04-24 18:28:42 -0700

    Move ballAnswer and answers into their own file.

--- test/tests.js
+++ test/tests.js
@@ -1,22 +1,10 @@
+import { ballAnswer, answers } from '../src/ball-answer.js';
 const test = QUnit.test;
 
 QUnit.assert.contains = function(needle, haystack, message) {
   var actual = haystack.indexOf(needle) > -1;
   this.push(actual, actual, needle, message);
 };
-
-const answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',
-  'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
-  'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don`t count on it.', 'My reply is no.',
-  'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
-
-function ballAnswer(question) {
-  if(question) {
-    let random = Math.floor(Math.random() * 20); 
-    return answers[random];
-  }
-  return 'Please ask a question.';
-}
 
 test('Send a question and expect an answer from the array of answers', function(assert) {
   //Arrange
