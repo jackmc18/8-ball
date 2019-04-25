4640c275c3f900bdeeee57ade00a64af65d9e945
Jackmc <jackmc1122@gmail.com>
2019-04-24 17:35:11 -0700

    Add 8-ball answers.

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,10 @@
 const test = QUnit.test;
 
+const answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.',
+  'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
+  'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Don`t count on it.', 'My reply is no.',
+  'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
+
 test('time to test a function', function(assert) {
   //Arrange
   // Set up your parameters and expectations
