3e94cbd52ce2298b5785ca44d02dd4b0f31bd90f
Jackmc <jackmc1122@gmail.com>
2019-04-24 20:27:04 -0700

    Make a test for a white space question..

--- test/tests.js
+++ test/tests.js
@@ -29,3 +29,15 @@ test('Send a blank question and expect "Please ask a question"', function(assert
   //Assert
   assert.equal(result, expected);
 });
+
+test('Send a white space question and expect "Please ask a question"', function(assert) {
+  //Arrange
+  // Set up your parameters and expectations
+  const question = '   ';
+  const expected = 'Please ask a question.';
+  //Act 
+  const result = ballAnswer(question);
+  // Call the function you're testing and set the result to a const
+  //Assert
+  assert.equal(result, expected);
+});
